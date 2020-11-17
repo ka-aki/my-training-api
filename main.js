f82982111f97d2adebf9b485b1ac2c801df84731
Aki Kahamura <aki.kahamura@sharefull.com>
2020-11-17 17:41:24 +0900

    add: implement csv

--- main.js
+++ main.js
@@ -12,28 +12,63 @@ const stringify = require("csv-stringify/lib/sync");
   const page = await browser.newPage();
   await page.goto("https://shotworks.jp/sw/list/a_01/mj_13/work?sv=");
 
-  const rows = await page.$$(
-    "#list > div > div.workinfo_inner > div.job_sum_wrapper > ul"
-  );
+  const rows = await page.$$("#list > div.workinfo_wrapper");
 
   var datas = [];
   for (let i = 0; i < rows.length; i++) {
-    const categoryJson = await convertElementToJson(rows[i], "li.job");
-    const salaryJson = await convertElementToJson(rows[i], "li.salary");
-    const workdayJson = await convertElementToJson(rows[i], "li.workday");
+    const categoryJson = await convertElementToJson(
+      rows[i],
+      "div.workinfo_inner > div.job_sum_wrapper > ul > li.job"
+    );
+    const salaryJson = await convertElementToJson(
+      rows[i],
+      "div.workinfo_inner > div.job_sum_wrapper > ul > li.salary"
+    );
+    const workdayJson = await convertElementToJson(
+      rows[i],
+      "div.workinfo_inner > div.job_sum_wrapper > ul > li.workday"
+    );
+    const titleJson = await convertElementToJson(
+      rows[i],
+      "div.catch_copy > div > h2 > a"
+    );
+    const companyJson = await convertElementToJson(
+      rows[i],
+      "div.workinfo_inner > div.com_info > div"
+    );
 
     let data = {
+      index: i + 1,
+      title: titleJson.trim(),
+      company: companyJson.trim(),
       category: categoryJson,
       salary: salaryJson,
       workday: workdayJson,
     };
     datas.push(data);
   }
-  console.log(datas, "datas");
+
+  const propertyNames = Object.getOwnPropertyNames(datas[0]);
+  const columns = {};
+  for (let name of propertyNames) {
+    columns[name] = name;
+  }
+
+  const csvString = stringify(datas, {
+    header: true,
+    columns,
+    quoted_string: true,
+  });
+  try {
+    fs.writeFileSync("./main.csv", csvString);
+    console.log("🎉 output complete!");
+  } catch (error) {
+    console.log("エラー：", error);
+  }
 })();
 
-async function convertElementToJson(listItem, selector) {
+const convertElementToJson = async (listItem, selector) => {
   const el = await listItem.$(selector);
-  const elValue = await el.getProperty("textContent");
-  return await elValue.jsonValue();
-}
+  const value = await el.getProperty("textContent");
+  return await value.jsonValue();
+};
